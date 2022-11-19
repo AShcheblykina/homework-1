610cfd9bc96438eac23752df11540c1bc3beb012
Anastasia Shcheblykina <anastasia23121993@gmail.com>
2022-11-19 20:08:35 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int mile = 20;
+        int amount = 5000;
+        int bonus = amount / mile;{
+        }
+            {
+                System.out.println("Итоговый бонус: " + bonus);
+            }
+}
+}
