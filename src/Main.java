3a145efff11d2a7c39b367b188076e217cfafe59
Anastasia Shcheblykina <anastasia23121993@gmail.com>
2022-11-23 18:30:35 +0300

    changed main

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         int mile = 20;
-        int amount = 5000;
-        int bonus = amount / mile;{
+        int amount = 10_000;
+        int bonus = amount / mile;
+        {
         }
-            {
-                System.out.println("Итоговый бонус: " + bonus);
-            }
-}
+        {
+            System.out.println("Бонусные мили: " + bonus);
+        }
+    }
 }
